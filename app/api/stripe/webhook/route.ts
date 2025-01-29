6f735a40ae96677f3d7f8a6a82364c985203872a
vitorbruno404 <vitor.bruno@vitorbruno.com>
2025-01-29 14:32:54 +0100

    still acacia problems

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 import { PrismaClient } from '@prisma/client';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-01-01',
+  apiVersion: '2025-01-27.acacia',
 });
 const prisma = new PrismaClient();
 
